4a55ac3854a4fb54c9136eb2277640ca7d084196
Odiljon Akhmedov <akhmedoff.odil@gmail.com>
2014-06-25 10:18:19 -0400

    Something just happened
    Surprize mothef***

---
+++ MVCforGit/MVCforGit/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCforGit.MvcApplication" Language="C#" %>
---
+++ MVCforGit/MVCforGit/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCforGit.Startup))]
+namespace MVCforGit
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
